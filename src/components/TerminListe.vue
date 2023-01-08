75d79291edce255eecb6578e183e2378251d86b3
thuyt <ThiThanhThuy.Nguyen@Student.HTW-Berlin.de>
2023-01-08 20:35:33 +0100

    Termin löschen

--- src/components/TerminListe.vue
+++ src/components/TerminListe.vue
@@ -12,6 +12,7 @@
         <th scope="col"> Geburtsdatum</th>
         <th scope="col"> Telefonnummer</th>
         <th scope="col"> TerminDatum</th>
+        <th scope="col"> Delete</th>
       </tr>
       </thead>
       <tbody>
@@ -22,6 +23,7 @@
         <td> {{t.kunde.geburtsDatum}}</td>
         <td> {{t.kunde.telefonnummer}}</td>
         <td> {{t.termindate}}</td>
+        <td><button class="btn btn-secondary me-3" type="submit" @click.prevent="deleteTermin(t.id)">Delete</button></td>
       </tr>
       </tbody>
     </table>
@@ -33,6 +35,20 @@ export default {
   data () {
     return {
       termine: []
+    }
+  },
+  methods: {
+    deleteTermin (id) {
+      const endpoint1 = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/termine' + '/' + id
+      const requestOptions = {
+        method: 'DELETE',
+        redirect: 'follow'
+      }
+
+      fetch(endpoint1, requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error))
     }
   },
   mounted () {
